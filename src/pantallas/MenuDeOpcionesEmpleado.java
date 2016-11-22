9f1a95029c022e07b455337c27b25a5c1c7f7411
German <germaraz@outlook.com>
2016-11-22 17:59:26 -0300

    modificado xml

--- src/pantallas/MenuDeOpcionesEmpleado.java
+++ src/pantallas/MenuDeOpcionesEmpleado.java
@@ -85,7 +85,7 @@ public class MenuDeOpcionesEmpleado extends javax.swing.JFrame {
         jToolBar1.setOpaque(false);
 
         jButton1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Clientes30.png"))); // NOI18N
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/productos25.png"))); // NOI18N
         jButton1.setText("Productos");
         jButton1.setFocusable(false);
         jButton1.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
@@ -305,33 +305,29 @@ public class MenuDeOpcionesEmpleado extends javax.swing.JFrame {
     }//GEN-LAST:event_btnVentaActionPerformed
 
     private void btnAyudaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAyudaActionPerformed
-            setLocationRelativeTo(null);
             ponLaAyuda();
             
     }//GEN-LAST:event_btnAyudaActionPerformed
     
     private void ponLaAyuda() {
-		try {
-			// Carga el fichero de ayuda
-			File fichero = new File("help/help_set.hs");
-			URL hsURL = fichero.toURI().toURL();
+        try {
+            // Carga el fichero de ayuda
+            File fichero = new File("help/help_set.hs");
+            URL hsURL = fichero.toURI().toURL();
 
-			// Crea el HelpSet y el HelpBroker
-			HelpSet helpset = new HelpSet(getClass().getClassLoader(), hsURL);
-			HelpBroker hb = helpset.createHelpBroker();
-                        
+            // Crea el HelpSet y el HelpBroker
+            HelpSet helpset = new HelpSet(getClass().getClassLoader(), hsURL);
+            HelpBroker hb = helpset.createHelpBroker();
 
-			// Pone ayuda a item de menu al pulsarlo y a F1 en ventana
-			// principal y secundaria.
-			hb.enableHelpOnButton(btnAyuda, "aplicacion", helpset);
-			hb.enableHelpKey(getRootPane(),"",helpset);
-        //(principal.getContentPane(), "ventana_principal", helpset);
-			
+            // Pone ayuda a item de menu al pulsarlo y a F1 en ventana
+            // principal y secundaria.
+            hb.enableHelpOnButton(btnAyuda, "aplicacion", helpset);
+            hb.enableHelpKey(getRootPane(),"",helpset);
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+}
     
     private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
         cerrar();
